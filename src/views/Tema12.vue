8be401a917c48010dd9332c359894883464f1895
mafecho-sena <maechavarriaorsena@gmail.com>
2026-03-04 20:52:09 -0500

    commit tema 12

--- src/views/Tema12.vue
+++ src/views/Tema12.vue
@@ -7,11 +7,273 @@
         span 12
       h1 Restablecimiento de derechos y seguimiento 
 
+    .row.align-items-start.mt-4
+      .col-lg-3.mb-lg-0.mb-4
+        figure(data-aos="fade-down")
+          img(src='@/assets/curso/tema12/1.svg', style='width: 290px').m-auto
+      
+      .col
+        p El restablecimiento de derechos constituye un proceso administrativo integral orientado a garantizar la protección efectiva de niños, niñas y adolescentes víctimas de violencias sexuales. Este proceso busca cesar la vulneración, prevenir su repetición y asegurar el ejercicio pleno de los derechos afectados, mediante acciones coordinadas entre las entidades competentes. Su desarrollo exige articulación intersectorial y actuación con debida diligencia reforzada por parte del Estado.
+
+        p Para comprender su aplicación y alcance, es necesario precisar el concepto de restablecimiento de derechos, así como sus características esenciales dentro del marco de protección integral:
+
+    .row.bg-112
+      .col-lg-12
+        .p-5 
+          .titulo-tres.mt-5: h3 Concepto de restablecimiento de derechos
+
+          .row.align-items-center
+            .col-lg-9
+              p El restablecimiento de derechos se define como el conjunto de decisiones administrativas, acciones intersectoriales y medidas de protección orientadas a cesar la vulneración, prevenir su repetición y garantizar el ejercicio pleno de los derechos de los niños, niñas y adolescentes afectados por violencias sexuales.
+
+              p Este proceso se caracteriza por:
+
+              .row.justify-content-center.mt-4
+                .row.justify-content-center
+                  .col-lg-3.d-flex.mt-4.mt-lg-0
+                    .crd.bg-base.bg-r8.p-4.h-100.w-100
+                      figure.mb-2(data-aos="zoom-in")
+                        img(src='@/assets/curso/tema12/2.svg', style='width: 40px')
+                      p.mb-0 Derecho a la vida y a la integridad física y mental.
+                
+                  .col-lg-3.d-flex.mt-4.mt-lg-0
+                    .crd.bg-base.bg-r8.p-4.h-100.w-100
+                      figure.mb-2(data-aos="zoom-in")
+                        img(src='@/assets/curso/tema12/3.svg', style='width: 40px')
+                      p.mb-0 Derecho a la dignidad humana.
+                
+                  .col-lg-3.d-flex.mt-4.mt-lg-0
+                    .crd.bg-base.bg-r8.p-4.h-100.w-100
+                      figure.mb-2(data-aos="zoom-in")
+                        img(src='@/assets/curso/tema12/4.svg', style='width: 40px')
+                      p.mb-0 Derecho a la libertad y a la autonomía personal.
+              
+                  .col-lg-3.d-flex.mt-4.mt-lg-0
+                    .crd.bg-base.bg-r8.p-4.h-100.w-100
+                      figure.mb-2(data-aos="zoom-in")
+                        img(src='@/assets/curso/tema12/5.svg', style='width: 20px')
+                      p.mb-0 Derecho a la igualdad y a la no discriminación.
+                
+
+            .col-lg-3.d-lg-block.d-none
+              figure
+                img(src='@/assets/curso/tema12/7.png', alt='', style="width: 396px").m-auto
+
+          p.mt-4 Además, implica una evaluación integral de las condiciones del niño, niña o adolescente, que incluye su entorno familiar, social, educativo y comunitario, así como la identificación de factores de riesgo y de protección que orienten la adopción de medidas adecuadas.
+
+    .row.justify-content-center.mt-4
+      .col-lg-10
+        .row.align-items-center
+          .col-lg-12
+            .cajon.color2.p-4
+              p La competencia para adelantar este proceso recae en las autoridades administrativas definidas por la normativa vigente.
+
+
+
+    separador
+    #t_12_1.titulo-segundo.color-acento-contenido
+      h2 12.1 Autoridades competentes
+
+    .row.align-items-start.mt-4
+      .col-lg-4.mb-lg-0.mb-4
+       figure(data-aos="fade-down")
+          img(src='@/assets/curso/tema12/7.png', style='width: 395px').m-auto 
+
+      .col
+         p El proceso de restablecimiento de derechos es competencia exclusiva de las autoridades administrativas de protección, conforme a lo establecido en la Ley 1098 de 2006 (Código de la Infancia y la Adolescencia). Estas autoridades tienen la responsabilidad de iniciar, dirigir y garantizar las medidas necesarias para proteger y restituir los derechos vulnerados de niños, niñas y adolescentes. A continuación, se presentan las entidades competentes encargadas de su implementación:
+
+         .row.align-items-center.mt-4.bg-base8.bg-r8.p-3
+            .col-lg-2
+              figure(data-aos="fade-down")
+                img(src='@/assets/curso/tema12/8.svg', style='width: 100px').m-auto
+            
+            .col          
+              ul.lista-ul--color
+                li.d-flex.align-items-start
+                  span.icono-img
+                  p.mb-0 Defensorías de Familia del ICBF
+
+      
+                li.d-flex.align-items-start
+                  span.icono-img
+                  p.mb-0 Comisarías de Familia
+
+      
+                li.d-flex.align-items-start
+                  span.icono-img
+                  p.mb-0 Inspectores de Policía (en ausencia de las anteriores)
+
+      
+                li.d-flex.align-items-start
+                  span.icono-img
+                  p.mb-0 Autoridades indígenas
+
+         p.mt-4 Estas autoridades tienen la facultad de iniciar el proceso administrativo de restablecimiento de derechos, adoptar las medidas de protección necesarias, realizar seguimiento y modificar dichas medidas cuando las circunstancias lo requieran, así como coordinar acciones con los sectores de salud, educación, justicia y protección social para garantizar la protección integral de niños, niñas y adolescentes.
+
+    .titulo-tres.mt-5: h3 Medidas administrativas de restablecimiento de derechos
+
+    .row.align-items-center
+      .col-lg-8
+        .row.align-items-start.mt-4
+          .col-lg-2.mb-lg-0.mb-4
+            figure(data-aos="fade-down")
+              img(src='@/assets/curso/tema12/9.svg', style='width: 84px').m-auto
+          
+          .col
+            p Las medidas administrativas de restablecimiento de derechos son decisiones adoptadas por la autoridad competente con el fin de proteger al NNA y garantizar su bienestar integral, atendiendo al nivel de riesgo identificado.
+
+        .row.mt-4.bg-base9.p-2.bg-r8
+          .col-12
+
+            p Principales medidas
+
+            ul.lista-ul--color
+              li.d-flex.align-items-start
+                span.icono-img
+                p.mb-0 Ubicación en medio familiar protector.
+
+
+              li.d-flex.align-items-start
+                span.icono-img
+                p.mb-0 Separación temporal del agresor.
+
+
+              li.d-flex.align-items-start
+                span.icono-img
+                p.mb-0 Ubicación en hogar sustituto o institución de protección.
+
+
+              li.d-flex.align-items-start
+                span.icono-img
+                p.mb-0 Garantía de atención integral en salud física y mental.
+
+              li.d-flex.align-items-start
+                span.icono-img
+                p.mb-0 Acceso y permanencia en el sistema educativo.
+
+              li.d-flex.align-items-start
+                span.icono-img
+                p.mb-0 Acompañamiento psicosocial individual y familiar.
+
+              li.d-flex.align-items-start
+                span.icono-img
+                p.mb-0 Seguimiento periódico del caso.      
+
+      .col-lg-4.d-lg-block.d-none
+        figure
+          img(src='@/assets/curso/tema12/10.png', alt='', style="width: 390px").m-auto
+
+
+    separador
+    #t_12_1.titulo-segundo.color-acento-contenido
+      h2 12.2 Seguimiento intersectorial
+
+    .row.align-items-center
+      .col-lg-9
+        p El seguimiento intersectorial constituye un proceso continuo y coordinado orientado a verificar la efectividad de las medidas adoptadas y garantizar la continuidad de la atención integral. Se fundamenta en los principios de corresponsabilidad institucional, enfoque de derechos, confidencialidad y no revictimización.
+
+        .row.align-items-center.mt-4.bg-base10.bg-r8.p-3
+          .col-lg-2.mb-lg-0.mb-4
+            figure(data-aos="fade-down")
+              img(src='@/assets/curso/tema12/11.svg', style='width: 100px').m-auto
+          
+          .col
+            p Uno de sus componentes centrales es la verificación de las decisiones adoptadas por las autoridades competentes. En este marco, el seguimiento permite verificar la vigencia y eficacia de las medidas de protección, identificar riesgos nuevos o persistentes, evaluar la necesidad de ajustes o fortalecimiento de las medidas, articular acciones con las autoridades responsables de justicia y protección, y realizar el registro sistemático de la evolución del caso.  
+
+        p.mt-4 El seguimiento intersectorial implica verificar el cumplimiento de las responsabilidades institucionales y asegurar la continuidad del acompañamiento para evitar nuevas vulneraciones:  
+
+
+      .col-lg-3.d-lg-block.d-none
+        figure
+          img(src='@/assets/curso/tema12/12.png', alt='', style="width: 290px").m-auto
+
+    
+
+    .row.align-items-center.mt-4.justify-content-center
+      .col-lg-10
+        .row.mt-2.p-3.bg-15.bg-r8.align-items-end
+          .col-8
+            .titulo-fondo: h4 Verificación del cumplimiento interinstitucional
+            
+            p.mt-3 Este proceso busca asegurar que cada entidad cumpla con las responsabilidades asignadas dentro de la ruta de atención.
+            
+            p.mt-3 Elementos de verificación
+            
+            ul.lista-ul--color
+              li.d-flex.align-items-start
+                span.icono-img
+                p.mb-0 Cumplimiento de las remisiones realizadas.
+
+              li.d-flex.align-items-start
+                span.icono-img
+                p.mb-0 Acceso efectivo a los servicios asignados.
+
+              li.d-flex.align-items-start
+                span.icono-img
+                p.mb-0 Coordinación oportuna entre sectores.
+                
+              li.d-flex.align-items-start
+                span.icono-img
+                p.mb-0 Identificación de fallas o vacíos en la atención.
+                
+              li.d-flex.align-items-start
+                span.icono-img
+                p.mb-0 Activación de acciones correctivas cuando sea necesario.
+
+
+          .col-lg-4.d-none.d-lg-block
+            figure(data-aos="fade-up")
+              img(src='@/assets/curso/tema12/13.svg', alt='', style="width: 206px").m-auto   
+
+
+    p.mt-4 A continuación, se presentan los elementos que garantizan la efectividad del seguimiento intersectorial, mediante la continuidad del acompañamiento institucional a la víctima y la articulación permanente entre las entidades responsables:
+
+    .row.justify-content-center.mt-4 
+      .col-lg-10
+        .row.justify-content-center.mt-4
+          .col-lg-5.mb-4.mb-lg-0
+            .tarjeta.clr-paleYellow.p-4.text-center.bg-base6.h-100
+              .row.justify-content-center.mb-3
+                .col-auto
+                  .circle-morado
+                    figure(data-aos="zoom-in")
+
+                    titulo-fondo Continuidad del acompañamiento intersectorial
+                    
+              p.text-start De acuerdo con la Organización Mundial de la Salud (OMS), la violencia sexual se define como:
+    
+              p.text-start Todo acto sexual, intento de consumar un acto sexual, comentarios o insinuaciones sexuales no deseadas, o acciones orientadas a comercializar, utilizar o controlar la sexualidad de una persona, mediante coerción, intimidación, amenaza, fuerza física, abuso de poder u otros mecanismos que anulen o limiten la voluntad personal, independientemente de la relación entre la víctima y el agresor y del ámbito en el que ocurra.
+    
+          .col-lg-5.mb-4.mb-lg-0
+            .tarjeta.clr-paleYellow.p-4.text-center.bg-12.h-100
+              .row.justify-content-center.mb-3
+                .col-auto
+                  .circle-morado
+                    figure(data-aos="zoom-in")
+                     
+              p.text-start En el marco normativo colombiano, la Ley 1257 de 2008 define la violencia sexual como:
+    
+              p.text-start Toda acción que cause daño
+
+    
+
+
+
+
+
+
+
+
+
+    
+
+
+
 </template>
 
 <script>
 export default {
-  name: 'Tema3',
+  name: 'Tema12',
   data: () => ({
     // variables de vue
   }),
